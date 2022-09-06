2342562f921078b1aded07760ae5b0a5c5038b52
Yaser Odat <yaserodat@outlook.com>
2022-09-06 20:48:06 +0300

    added middleware

---
+++ server.js
@@ -0,0 +1,37 @@
+'use strict'
+
+const express = require('express');
+const app = express();
+const cors = require('cors')
+require('dotenv').config
+const squareCheck = require('./middlewares/validate-number.js')
+const errorHandler = require('./error-handlers/500');
+
+const port = process.env.PORT || 4001
+
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.status(200).send('Home Page')
+})
+
+app.get('/square', squareCheck(2), (req, res) => {
+    
+    const squaredNumber = req.square;
+    
+    res.send(squaredNumber)
+})
+
+
+app.use(errorHandler);
+
+function start(port) {
+app.listen(port, () => console.log('The server is up on port',port ))
+}
+
+module.exports = {
+    app: app,
+    start: start
+  }
